617ee234453b868a72fe32834d051b734ed2958c
Kelvin Herwanda Tandrio <herwandatandrio@gmail.com>
2021-02-08 23:17:01 +0700

    Add Module Common, Core, and Category

---
+++ Common/Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Kelvin HT on 2/8/21.
+//  Copyright © 2021 Kelvin HT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
